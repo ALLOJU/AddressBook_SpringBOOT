bba1d5116efa4db4287cf7e2fbff1613b8aabb68
ALLOJU <mounika.alloju@gmail.com>
2022-05-26 10:17:48 +0530

    Lombok loggings added

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,16 +1,22 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 
     public static void main(String[] args)
     {
-        System.out.println("Welcome to bridgelabz");
-        SpringApplication.run(AddressBookAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("AddressBook App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("AddressBook App DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
